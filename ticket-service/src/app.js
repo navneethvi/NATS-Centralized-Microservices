a7372d33bff1a5f14bc858cd706eaa16faacaa74
Navaneeth V <navaneethvinod18@gmail.com>
2024-06-30 17:02:14 +0530

    Ticket Service : Listens to the events

--- ticket-service/src/app.js
+++ ticket-service/src/app.js
@@ -1,10 +1,10 @@
 import express from "express";
-import session from 'express-session'
+import session from "express-session";
 import errorHandler from "./middleware/errorhandler.js";
 import mongoose from "mongoose";
 import router from "./routers/router.js";
 
-import natsWrapper from './nats/nats-client.js'
+import natsWrapper from "./nats/nats-client.js";
 
 const app = express();
 
@@ -39,7 +39,13 @@ const connectDB = async () => {
     throw new Error("JWT_KEY must be defined");
   }
   try {
-    await natsWrapper.connect('ticketing', 'abc', 'http://nats-svc:4222')
+    await natsWrapper.connect("ticketing", "abc", "http://nats-svc:4222");
+    natsWrapper.getClient().on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
+    process.on("SIGINT", () => natsWrapper.getClient().close());
+    process.on("SIGTERM", () => natsWrapper.getClient().close());
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Database Connected");
   } catch (error) {
--- ticket-service/src/middleware/auth.js
+++ ticket-service/src/middleware/auth.js
@@ -23,3 +23,4 @@ const isLogin = async (req, res, next) => {
 };
 
 export default isLogin;
+ 
